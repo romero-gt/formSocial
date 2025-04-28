60976fc9db1665c5a27e28ef3976d17d94cd6255
Flávia Reis <flaviarster@gmail.com>
2025-04-28 12:07:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const form = document.getElementById("socialForm");
+const resultado = document.getElementById("resultado");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Captura de Dados
+  const nome = document.getElementById("nome").value;
+  const sobrenome = document.getElementById("sobrenome").value;
+  const nascimento = document.getElementById("nascimento").value;
+  const estadoCivil = document.getElementById("estadoCivil").value;
+  const sexo = document.querySelector('input[name="sexo"]:checked').value;
+  const opcaoSexual = document.getElementById("opcaoSexual").value;
+
+  // Alerta para dizer que a data de nascimento é obrigatória
+  if (!nascimento) {
+    alert("Data de nascimento é obrigatória!");
+    return;
+  }
+
+  // Calculando a idade
+  const dataNascimento = new Date(nascimento);
+  const idade = calcularIdade(dataNascimento);
+
+  function calcularIdade(dataNascimento) {
+    const hoje = new Date();
+    let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+
+    if (
+      hoje.getMonth() < dataNascimento.getMonth() ||
+      (hoje.getMonth() === dataNascimento.getMonth() &&
+        hoje.getDate() < dataNascimento.getDate())
+    ) {
+      idade--;
+    }
+    return idade;
+  }
+
+  // Definindo a geração:
+  let geracao;
+
+  if (dataNascimento.getFullYear() > 1946 && dataNascimento.getFullYear() < 1964) {
+    geracao = 'Geração Baby Bloomer'
+  } else if (dataNascimento.getFullYear() >= 1965 && dataNascimento.getFullYear() <= 1980) {
+   geracao = 'Geração X'
+  } else if (dataNascimento.getFullYear() >= 1981 && dataNascimento.getFullYear() <= 1996) {
+    geracao = 'Millennial'
+  } else if (dataNascimento.getFullYear() >= 1997 && dataNascimento.getFullYear() <= 2010) {
+    geracao = 'Geração Z'
+  } else if (dataNascimento.getFullYear() > 2010) {
+    geracao = 'Geração Alpha'
+  } else {
+    geracao = 'Nasceu antes de 1946'
+  }
+
+  // Mostrando o resultado
+  resultado.innerHTML = `
+    <div class="card p-4 shadow-sm">
+      <h4>Cadastro Realizado:</h4>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${nome} ${sobrenome}</li>
+        <li class="list-group-item">${idade}</li>
+        <li class="list-group-item">${geracao}</li>
+        <li class="list-group-item">${sexo}</li>
+        <li class="list-group-item">${opcaoSexual}</li>
+        <li class="list-group-item">${estadoCivil}</li>
+      </ul> 
+    </div>
+  `;
+});
