2e594f4fc3e0a99ea6d41926efc97e13133eb9da
Flávia Reis <flaviarster@gmail.com>
2025-04-29 09:12:36 -0300

    feat: adiciona validação de nome e sobrenome

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Captura de Dados
-  const nome = document.getElementById("nome").value;
-  const sobrenome = document.getElementById("sobrenome").value;
+  const nome = document.getElementById("nome").value.trim().toUpperCase();
+  const sobrenome = document.getElementById("sobrenome").value.toUpperCase();
   const nascimento = document.getElementById("nascimento").value;
-  const estadoCivil = document.getElementById("estadoCivil").value;
-  const sexo = document.querySelector('input[name="sexo"]:checked').value;
-  const opcaoSexual = document.getElementById("opcaoSexual").value;
+  const estadoCivil = document
+    .getElementById("estadoCivil")
+    .value.toUpperCase();
+  const sexo = document
+    .querySelector('input[name="sexo"]:checked')
+    .value.toUpperCase();
+  const opcaoSexual = document
+    .getElementById("opcaoSexual")
+    .value.toUpperCase();
+  const erro = document.getElementById("nome-erro");
+
+  const regexTexto = /^[A-Za-zÀ-ù\s]+$/;
+  if (!regexTexto.test(nome) || !regexTexto.test(sobrenome)) {
+    erro.innerText = "Nome e sobrenome devem conter apenas letras!";
+    return;
+  }
 
   // Alerta para dizer que a data de nascimento é obrigatória
   if (!nascimento) {
@@ -38,7 +51,7 @@ form.addEventListener("submit", (e) => {
 
   // Validando a idade:
   if (idade < 10 || idade > 90) {
-    alert('Idade inválida! Deve ser entre 10 e 90 anos!')
+    alert("Idade inválida! Deve ser entre 10 e 90 anos!");
   }
 
   // Definindo a geração:
@@ -75,13 +88,16 @@ form.addEventListener("submit", (e) => {
     <div class="card p-4 shadow-sm">
       <h4>Cadastro Realizado:</h4>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">${nome.toUpperCase()} ${sobrenome.toUpperCase()}</li>
+        <li class="list-group-item">${nome} ${sobrenome}</li>
         <li class="list-group-item">${idade}</li>
         <li class="list-group-item">${geracao.toUpperCase()}</li>
-        <li class="list-group-item">${sexo.toUpperCase()}</li>
-        <li class="list-group-item">${opcaoSexual.toUpperCase()}</li>
-        <li class="list-group-item">${estadoCivil.toUpperCase()}</li>
+        <li class="list-group-item">${sexo}</li>
+        <li class="list-group-item">${opcaoSexual}</li>
+        <li class="list-group-item">${estadoCivil}</li>
       </ul> 
     </div>
   `;
+
+  form.reset();
+  erro.innerText = "";
 });
