e20c145c56db96bd2a82c17e8ecee50ab180ba62
Diego Andrade <Diegoandradeoliveira33@gmail.com>
2025-04-29 09:44:56 -0300

    Update script.js
    atualização script.js

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ form.addEventListener('submit', function (event) {
     let sexo = document.querySelector('input[name="sexo"]:checked').value;
     let opcaoSexual = document.getElementById('opcaoSexual').value;
 
+    const regexTexto = /^[A-Za-zÁ-á\s]+$/;
+    if (!regexTexto.test(nome) || !regexTexto.test(sobrenome)){
+        alert('nome e sobre nome devem conter apenas letras')
+    }
+
+
     if (!nascimento) {
         alert('Data de nascimento é obrigatória!');
         return;
@@ -19,6 +25,13 @@ form.addEventListener('submit', function (event) {
 
     const dataNascimento = new Date(nascimento);
     const idade = calcularIdade(dataNascimento);
+
+    if(idade < 10 || idade > 90) {
+        alert("idade invalida! usuario deve ter entre 10 e 90 anos.")
+    }
+
+    const Geracao = identificarGeracao(dataNascimento.getFullYear());
+    
 
     // Mostrando o resultado
     resultado.innerHTML = `
@@ -28,6 +41,7 @@ form.addEventListener('submit', function (event) {
                     <li class="list-group-item">${nome} ${sobrenome}</li>
                     <li class="list-group-item">${idade} anos</li>
                     <li class="list-group-item">${sexo}</li>
+                    <li class="list-group-item">${Geracao}</li>
                     <li class="list-group-item">${opcaoSexual}</li>
                     <li class="list-group-item">${estadoCivil}</li>
                 </ul>
@@ -44,4 +58,22 @@ function calcularIdade(dataNascimento) {
         idade--;
     }
     return idade;
+}
+
+function identificarGeracao(){
+    switch (true){
+        case ano >= 1946 && ano <=1964:
+            return 'Baby Boomer';
+        case ano >= 1965 && ano <=1980:
+            return 'Geracao x';
+        case ano >= 1981 && ano <=1996:
+            return ' Milleniun (Geracao y';
+        case ano >= 1997 && ano <=2012:
+            return 'centennial (Geracao Z)';
+        case ano >= 2013:
+            return 'Geracao Alpha';
+            default:
+                return 'Geracao nao Classificada';                
+            
+    }
 }
