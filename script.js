938d9ecb91a07c7579ab9ad5b26847a1bd29ff2e
luiz emanuel <luiz.mascarenhas80@gmail.com>
2025-04-29 09:07:20 -0300

    Atualização

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ form.addEventListener('submit', function (event){
     let nome = document.getElementById('nome').value;
     let sobrenome = document.getElementById('sobrenome').value;
     let nascimento = document.getElementById('nascimento').value;
-    let estadoCivil = document.getElementById('estadoCivel').value;
+    let estadoCivil = document.getElementById('estadoCivil').value;
     let sexo = document.querySelector('input[name="sexo"]:checked').value;
-    let opcaoSexual = document.getElementById('opcaoSexual').value;
+    let opcaoSexual = document.getElementById('opcaoSexual').value
+
+    const regexTexto = /^[A-Za-zÀ-ú]+$/;
+    if (!regexTexto.test(nome) || !regexTexto.test(sobrenome)) {
+        alert('Nome e sobrenome devem conter apenas letras. ')
+    }
 
     if (!nascimento){
         alert('Data de Nascimento é Obrigatória')
@@ -20,7 +25,11 @@ form.addEventListener('submit', function (event){
 
     const dataNascimento = new Date(nascimento);
     const idade = calcularIdade(dataNascimento);
+    const geracao = calcularGeracao(idade)
 
+    if (idade < 10 || idade > 90) {
+        alert("Idade inválida!")
+    }
 
     resultado.innerHTML = `
         <div class="card p-4 shadow-sm">
@@ -35,15 +44,29 @@ form.addEventListener('submit', function (event){
                 </ul>                    
         </div>
     `
+    function calcularIdade(dataNascimento) {
+        const hoje = new Date();
+        let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+        
+        if (hoje.getMonth() < dataNascimento.getMonth() || hoje.getMonth() === dataNascimento.getMonth() && hoje.getDate() < dataNascimento.getDate()
+        ) {
+            idade--;
+        }
+        return idade;
+    }
+
+    function calcularGeracao(idade) {
+        if (idade <= 25) {
+            return "Gereção z"
+        } else if (idade > 25 && idade <= 45) {
+            return "Geração Y"
+        } else {
+            return "Geração X"
+        }
+    }
+
 })
 
-function calcularIdade(dataNascimento) {
-    const hoje = new Date();
-    let idade = hoje.getFullYear() - dataNascimento.getFullYear();
-    
-    if (hoje.getMonth() < dataNascimento.getMonth() || hoje.getMonth() === dataNascimento.getMonth() && hoje.getDate() < dataNascimento.getDate()
-    ) {
-        idade--;
-    }
-    return idade;
-}
+
+
+
